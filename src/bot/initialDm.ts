913233d222cb654de37f26086f0a28f4c5e0d940
Ben Ennifer <TheDireChicken@gmail.com>
2020-10-20 16:56:19 +0100

    Update initialDm.ts
    reworded the initial DM to reflect how gamesoc runs the discord

--- src/bot/initialDm.ts
+++ src/bot/initialDm.ts
@@ -13,7 +13,7 @@ client.on('guildMemberAdd', async (member) => {
   await member.send(
     `Hey there <@!${
       (member.user as User).id
-    }> :wave:\nThanks for joining the GameSoc Discord Server!\nTo get started you need to verify your membership with me.\nPlease respond with your student ID number so I can verify that you are a GameSoc member.\nIf you believe this is in error, or something goes wrong, please contact the committee in <#${
+    }> :wave:\nThanks for joining the GameSoc Discord Server!\nAs you're new you can verify your membership with me now to get access to more features on the discord.\nPlease respond with your student ID number so I can verify that you are a GameSoc member.\nIf you believe this is in error, or something goes wrong, please contact the committee in <#${
       process.env.discordHelpChannelID as string
     }> for help.\nThanks,\nA GameSoc Bot.`,
   );
